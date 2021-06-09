1474f7ddcb6197959f51b23fc17bfd56fdb00147
Mohamed Abbas <mohamed71992@gmail.com>
2021-06-09 02:29:38 +0200

    adjust the app implementation

--- Meme Generator Project/src/app.py
+++ Meme Generator Project/src/app.py
@@ -1,10 +1,10 @@
 import random
 import os
 import requests
-import sys
-from flask import Flask, render_template, abort, request
-from io import BytesIO
-from PIL import Image
+
+from requests import RequestException
+from flask import Flask, render_template, request
+from pathlib import Path
 
 from QuoteEngine import QuoteModel, Ingestor
 from MemeGenerator import MemeEngine
@@ -22,12 +22,17 @@ def setup():
                    './_data/DogQuotes/DogQuotesPDF.pdf',
                    './_data/DogQuotes/DogQuotesCSV.csv']
 
-    quotes = []
-    for f in quote_files:
-        quotes.extend(Ingestor.parse(f))
+    quotes = list()
+    for file in quote_files:
+        quotes.extend(Ingestor.parse(file))
 
     images_path = "./_data/photos/dog/"
-    imgs = list(os.listdir(images_path))
+
+    # Read all files from the given path
+    imgs = list()
+    for root, dirs, files in os.walk(images_path):
+        for file in files:
+            imgs.append(os.path.join(root, file))
 
     return quotes, imgs
 
@@ -42,10 +47,7 @@ def meme_rand():
     img = random.choice(imgs)
     quote = random.choice(quotes)
 
-    images_path = "./_data/photos/dog/"
-
-    path = meme.make_meme(os.path.join(images_path, img),
-                          quote.body, quote.author)
+    path = meme.make_meme(img, quote.body, quote.author)
     return render_template('meme.html', path=path)
 
 
@@ -59,28 +61,26 @@ def meme_form():
 def meme_post():
     """ Create a user defined meme """
 
-    # @TODO:
-    # 1. Use requests to save the image from the image_url
-    #    form param to a temp local file.
-    # 2. Use the meme object to generate a meme using this temp
-    #    file and the body and author form paramaters.
-    # 3. Remove the temporary saved image.
-
+    # Read the form parameters
     img = request.form.get('image_url')
     body = request.form.get('body')
     author = request.form.get('author')
 
-    temp_path = os.path.join(os.path.dirname(__file__), "temp.jpg")
-    response = requests.get(img)
-    im = Image.open(BytesIO(response.content))
-    images_path = os.path.abspath("./_data/photos/dog/")
-    im.save(temp_path)
+    try:
+        response = requests.get(img, allow_redirects=True)
+    except RequestException:
+        print("Can't accees the given image URL")
 
-    print("-----------------------------------", file=sys.stdout)
-    print(os.path.join(images_path, img), file=sys.stderr)
+    # save the image in a temporary file
+    temp_file = Path(f'./tmp/{random.randint(0, 100000000)}.png')
+    temp_file.parent.mkdir(exist_ok=True)
+    open(temp_file, 'wb').write(response.content)
+
+    # create the meme using the temporary downloaded image
     quote = QuoteModel(body, author)
-    path = meme.make_meme(temp_path, quote.body, quote.author)
-    # os.remove(temp_path)
+    path = meme.make_meme(temp_file, quote.body, quote.author)
+
+    temp_file.unlink(missing_ok=True)
 
     return render_template('meme.html', path=path)
 
